5fc34b143c3105345fdacd47fe4ec6c7992d4ef1
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2017-04-17 20:28:30 -0700

    human input wip

--- humanPlayer.py
+++ humanPlayer.py
@@ -1,21 +1,24 @@
 from policy import Policy 
+from gameStatus import GameStatus
 import random
 import numpy as np
 
 
 class HumanPolicy(Policy):
-    def move(self, state):
-    	print ('human moves here')
+	def move(self, state):
+		print ('human moves here')
 
-    	idx = np.random.randint(len(legal_moves))
-    	print ('idx is: {}'.format(idx))
+		legal_moves = state.legal_moves()
+		idx = np.random.randint(len(legal_moves))
+		print ('idx is: {}'.format(idx))
+		print ('legal_moves[idx] is: {}'.format(legal_moves[idx])) 
 
-        legal_moves = state.legal_moves()
-        
-        print ('legal_moves[idx] is: {}'.format(legal_moves[idx])) 
-
-        policyInput = raw_input('Human, enter your next move: ')
-        
-        return legal_moves[idx]
+		policyInput = raw_input('Human, enter your next move: ')
+		# convert the string to a move
+		# Verify that the specified action is legal
+		# assert (row, col) in self.legal_moves()
+		# return the index of that move
+		
+		return legal_moves[idx]
 
 
